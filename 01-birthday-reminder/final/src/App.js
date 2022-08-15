564111676ab0e9405f2dfa14194321dc8c239dcb
Anirudh <sidhu.sidhu.anirudh4@gmail.com>
2022-08-15 14:44:13 +0530

    Made changes in birthday-remainder App.js

--- 01-birthday-reminder/final/src/App.js
+++ 01-birthday-reminder/final/src/App.js
@@ -1,14 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import data from './data'
 import List from './List'
 function App() {
   const [people, setPeople] = useState(data)
+
+  useEffect(() => {
+    console.log("Page Reloaded")
+  }, [])
+
+  useEffect(()=>{
+    console.log('List Changed');
+  }, [people])
+
   return (
     <main>
       <section className='container'>
         <h3>{people.length} birthdays today</h3>
         <List people={people} />
-        <button onClick={() => setPeople([])}>clear all</button>
+        <button onClick={() => setPeople(people.slice(1, people.length))}>Remove first</button>
       </section>
     </main>
   )
